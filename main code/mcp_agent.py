3355659eac8e7b4ba13aacf3cb50bbad744d663c
yuwen41 <vywntnu@gmail.com>
2025-09-05 14:44:25 +0800

    Update mcp_agent.py

--- main code/mcp_agent.py
+++ main code/mcp_agent.py
@@ -6,27 +6,27 @@ from langgraph.prebuilt import create_react_agent
 from langchain_core.messages.tool import ToolMessage
 
 
-#openai API key 設置
+#openai API key setting
 openai_api_key= os.getenv("OPENAI_API_KEY")
 
 if not openai_api_key:
-    raise ValueError("請先在環境變數中設定 OPENAI_API_KEY")
+    raise ValueError("Please set OPENAI_API_KEY in your environment variables first")
 
 os.environ["OPENAI_API_KEY"] = openai_api_key
 
-#字串編碼清理
+#String encoding cleanup
 def clean_input(text):
     return text.encode("utf-8", "ignore").decode("utf-8")
 
-#分析被取用的工具
+#Analyze the tools that were invoked
 def extract_invoked_tools(messages):
     tool_names = []
     for msg in messages:
         if isinstance(msg, ToolMessage):
-            tool_names.append(msg.name)# msg.name 為工具名稱
+            tool_names.append(msg.name)# msg.name is the tool name
     return tool_names
 
-#建立 Client
+#Build Client
 async def build_agent():
     client = MultiServerMCPClient({
         "multi": {
@@ -44,3 +44,4 @@ async def build_agent():
 
     agent = create_react_agent("openai:gpt-4.1", tools)
     return agent
+
